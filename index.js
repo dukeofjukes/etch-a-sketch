b274e62e0bd21aec9c82e693ee3da399a2924a6d
Brandon Burtchell <62345978+dukeofjukes@users.noreply.github.com>
2022-05-08 13:37:19 -0500

    Create resize grid functionality

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 const gridContainer = document.querySelector("#main-container");
 const resetBtn = document.querySelector("#reset-btn");
-const numRows = 64;
-const gridSize = numRows * numRows;
+const newGridBtn = document.querySelector("#new-grid-btn");
 
+let numRows = 64;
+let gridSize = numRows * numRows;
 let grid = initGrid();
 
 resetBtn.addEventListener("click", (e) => {
   resetGrid();
 });
 
+newGridBtn.addEventListener("click", (e) => {
+  resizeGrid();
+});
+
 function initGrid() {
   let grid = Array();
+
+  while (gridContainer.firstChild) {
+    gridContainer.removeChild(gridContainer.firstChild);
+  }
+  gridContainer.style.gridTemplateColumns = `repeat(${numRows}, 0fr)`;
 
   for (let i = 0; i < gridSize; i++) {
     const newGridDiv = document.createElement("div");
     newGridDiv.className = "grid-cell";
     newGridDiv.id = `grid-${i}`;
+    newGridDiv.style.width = `${960 / numRows}px`;
+    newGridDiv.style.height = `${960 / numRows}px`;
     gridContainer.appendChild(newGridDiv);
     grid.push(newGridDiv);
   }
@@ -33,4 +45,15 @@ function resetGrid() {
   grid.forEach((cell) => {
     cell.classList.remove("colored");
   });
+}
+
+function resizeGrid() {
+  let numRowsStr = prompt("Enter a grid width (1-100):", "64");
+  let prevNumRows = numRows;
+  numRows = parseInt(numRowsStr);
+  if (numRows > 100 || numRows < 0) {
+    numRows = prevNumRows;
+  }
+  gridSize = numRows * numRows;
+  grid = initGrid();
 }
