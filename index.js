7c72d421dad908c9f7148a4667dafc4ad1623c4d
Brandon Burtchell <62345978+dukeofjukes@users.noreply.github.com>
2022-05-08 12:57:15 -0500

    Rename vars and encapsulate grid init to a function

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const gridContainer = document.querySelector("#main-container");
+const resetBtn = document.querySelector("#reset-btn");
 
 const numRows = 16;
 const gridSize = numRows * numRows;
 
-let grid = Array();
-for (let i = 0; i < gridSize; i++) {
-  const newGridDiv = document.createElement("div");
-  newGridDiv.className = "grid";
-  newGridDiv.id = `grid-${i}`;
-  gridContainer.appendChild(newGridDiv);
-  grid.push(newGridDiv);
-}
+let grid = initGrid();
 
-grid.forEach((cell) => {
-  cell.addEventListener("mouseover", (e) => {
-    cell.classList.add("colored");
+function initGrid() {
+  let grid = Array();
+  for (let i = 0; i < gridSize; i++) {
+    const newGridDiv = document.createElement("div");
+    newGridDiv.className = "grid";
+    newGridDiv.id = `grid-${i}`;
+    gridContainer.appendChild(newGridDiv);
+    grid.push(newGridDiv);
+  }
+
+  grid.forEach((cell) => {
+    cell.addEventListener("mouseover", (e) => {
+      cell.classList.add("colored");
+    });
   });
-});
+}
